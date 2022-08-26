bf43c8e33dd57c428bd96f3bca101d501cc9521f
svalyavakilia <s21211@pjwstk.edu.pl>
2022-08-26 14:30:04 +0200

    upload my solution with 100% code coverage

--- src/main/java/implementation/SMSGate.java
+++ src/main/java/implementation/SMSGate.java
@@ -1,7 +1,31 @@
 package implementation;
 
+import java.util.Set;
+
 public class SMSGate {
+    private static final int MAXIMUM_MESSAGE_LENGTH = 160;
+    private static final String DOTS = "...";
+    private static final Set<Character> SPECIAL_CHARACTERS = Set.of(
+        '(', ')', '{', '}', '[', ']', '^', '\\'
+    );
+
     public String formatMessage(final String originalMessage) {
-        return "";
+        final int omLength = originalMessage.length();
+        final StringBuilder fmBuilder = new StringBuilder();
+        int i = 0, fmActualLength = 0;
+        char cc;
+        while ((i < omLength) && (fmActualLength <= MAXIMUM_MESSAGE_LENGTH)) {
+            fmBuilder.append(cc = originalMessage.charAt((i)++));
+            fmActualLength += (SPECIAL_CHARACTERS.contains(cc)) ? 2: 1;
+        }
+        if (fmActualLength > MAXIMUM_MESSAGE_LENGTH) {
+            while (fmActualLength > (MAXIMUM_MESSAGE_LENGTH - DOTS.length())) {
+                cc = fmBuilder.charAt(fmBuilder.length() - 1);
+                fmBuilder.deleteCharAt(fmBuilder.length() - 1);
+                fmActualLength -= (SPECIAL_CHARACTERS.contains(cc)) ? 2: 1;
+            }
+            fmBuilder.append(DOTS);
+        }
+        return fmBuilder.toString();
     }
 }
--- src/test/java/implementation/SMSGateTest.java
+++ src/test/java/implementation/SMSGateTest.java
@@ -3,12 +3,14 @@ package implementation;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class SMSGateTest {
     private final SMSGate smsGate = new SMSGate();
 
     @Test
-    public void testFormatMessageForMaximumLengthWithOnlyRegularCharacters() {
+    public void
+    testFormatMessageMaximumLengthMessageWithOnlyRegularCharacters() {
         // given (arrange, having)
         final String originalMessage = "Java".repeat(40);
 
@@ -17,5 +19,79 @@ public class SMSGateTest {
 
         // then (assert, then)
         assertThat(formattedMessage).isEqualTo(originalMessage);
+    }
+
+    @Test
+    public void testFormatMessageLongerMessageWithOnlyRegularCharacters() {
+        // given (arrange, having)
+        final String originalMessage = "abc".repeat(54);
+
+        // when (act, if)
+        final String formattedMessage = smsGate.formatMessage(originalMessage);
+
+        // then (assert, then)
+        assertThat(formattedMessage).isEqualTo("abc".repeat(52) + "a...");
+    }
+
+    @Test
+    public void testFormatMessageMaximumLengthMessageWithSpecialCharacters() {
+        // given (arrange, having)
+        final String originalMessage = "<>".repeat(72) + "(){}[]^\\";
+
+        // when (act, if)
+        final String formattedMessage = smsGate.formatMessage(originalMessage);
+
+        // then (assert, then)
+        assertThat(formattedMessage).isEqualTo(originalMessage);
+    }
+
+    @Test
+    public void testFormatMessageLongerMessageWithSpecialCharacters() {
+        // given (arrange, having)
+        final String originalMessage = "<>".repeat(76) + "&[]||||";
+
+        // when (act, if)
+        final String formattedMessage = smsGate.formatMessage(originalMessage);
+
+        // then (assert, then)
+        assertThat(formattedMessage).isEqualTo("<>".repeat(76) + "&[]...");
+    }
+
+    @Test
+    public void
+    testFormatMessageLongerMessageWithSpecialCharacterToBeTheLastDeleted() {
+        // given (arrange, having)
+        final String originalMessage = "<>".repeat(77) + "();;;";
+
+        // when (act, if)
+        final String formattedMessage = smsGate.formatMessage(originalMessage);
+
+        // then (assert, then)
+        assertThat(formattedMessage).isEqualTo("<>".repeat(77) + "(...");
+    }
+
+    @Test
+    public void testFormatMessageMessageReferencesEmptyString() {
+        // given (arrange, having)
+        final String originalMessage = "";
+
+        // when (act, if)
+        final String formattedMessage = smsGate.formatMessage(originalMessage);
+
+        // then (assert, then)
+        assertThat(formattedMessage).isEmpty();
+    }
+
+    @Test
+    public void testFormatMessageMessageReferencesNull() {
+        // given (arrange, having)
+        final String originalMessage = null;
+
+        // when (act, if)
+        // in this case, this part is merged with the following one
+
+        // then (assert, then)
+        assertThatThrownBy(() -> smsGate.formatMessage(originalMessage))
+            .isExactlyInstanceOf(NullPointerException.class);
     }
 }
